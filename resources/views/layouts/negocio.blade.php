bad508d63179f8575b8224114ed9085349d211f7
Juan Sebastian Cruz Perdomo <jcruz02@itfip.edu.co>
2017-12-05 11:45:56 -0500

    ajustes plantilla nueva

--- resources/views/layouts/negocio.blade.php
+++ resources/views/layouts/negocio.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.adminlte')
 
 
 @section('dropmenu')
@@ -8,22 +8,45 @@
 
 @endsection
 
+@section('sidebaruser')
+    @parent
+    <a href="#">{{Auth::user()->negocio->descripcion}}</a>
+@endsection
 @section('sidebar')
-    <h3>{{Auth::user()->negocio->descripcion}}</h3>
-    <ul class="nav side-menu">
-        <li><a href="/negocio"><i class="fa fa-home"></i> Usuarios</a></li>
-        <li><a href="/negocio/empresas"><i class="fa fa-building-o"></i> Empresas</a></li>
-        <li><a href="/negocio/proveedores"><i class="fa fa-handshake-o"></i> Proveedores</a></li>
-        <li><a href="/negocio/productos"><i class="fa fa-shopping-bag "></i> Productos</a></li>
-        <li><a><i class="fa fa-bar-chart-o"></i> Cotizaciones <span class="fa fa-chevron-down"></span></a>
-            <ul class="nav child_menu">
-            <li><a href="/negocio/oportunidades/abiertas">Cerradas</a></li>
-            <li><a href="/negocio/oportunidades/cerradas">Pendientes</a></li>
-            <li><a href="/negocio/oportunidades/seguimiento">Seguimientos</a></li>
-            </ul>
-        </li>
-        <li><a href="/negocio/cotizaciones"><i class="fa fa-file-word-o"></i> Reportes</a></li>
-    </ul>
+    
+    <li><a href="/negocio"><i class="fa fa-home"></i> <span>Usuarios</span></a></li>
+    <li><a href="/negocio/empresas"><i class="fa fa-building-o"></i> <span>Empresas</span></a></li>
+    <li><a href="/negocio/proveedores"><i class="fa fa-handshake-o"></i> <span>Proveedores</span></a></li>
+    <li><a href="/negocio/productos"><i class="fa fa-shopping-bag "></i> <span>Productos</span></a></li>
+    <li class="treeview">
+        <a href="#">
+        <i class="fa fa-bar-chart-o"></i> <span>Cotizaciones</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+        </a>
+        <ul class="treeview-menu">
+        <li><a href="/negocio/oportunidades/abiertas"><i class="fa fa-circle-o text-red"></i> Pendientes
+            <span class="pull-right-container">
+              <span class="label pull-right bg-red">4</span>
+            </span>
+        </a></li>
+        <li><a href="/negocio/oportunidades/seguimiento"><i class="fa fa-circle-o text-yellow"></i> Seguimientos
+            <span class="pull-right-container">
+              <span class="label pull-right bg-yellow">4</span>
+            </span>
+        </a></li>
+        <li><a href="/negocio/oportunidades/cerradas"><i class="fa fa-circle-o text-green"></i> Cerradas
+            <span class="pull-right-container">
+              <span class="label pull-right bg-green">4</span>
+            </span>
+        </a></li>
+        
+        </ul>
+    </li>
+    <li><a href="/negocio/cotizaciones"><i class="fa fa-file-word-o"></i> <span>Reportes</span></a></li>
+    
+   
 @endsection
 
 @section('content')
